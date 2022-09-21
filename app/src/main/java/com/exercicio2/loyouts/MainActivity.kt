5b93233afe58ac13792b9795bc419e69c1276e54
unknown <vitoria1065@outlook.com>
2022-09-21 19:07:48 -0300

    Exerc2

---
+++ app/src/main/java/com/exercicio2/loyouts/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.exercicio2.loyouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
